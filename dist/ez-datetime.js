fa545a9831bfa9b02146fde4433549d3bc984277
Floyd <kirinse@gmail.com>
2015-04-08 19:05:48 +0800

    * Popup start

--- dist/ez-datetime.js
+++ dist/ez-datetime.js
@@ -513,6 +513,134 @@ angular.module('ez.datetime').directive('ezDatetimeControl', [
   }
 ]);
 
+angular.module('ez.datetime').directive('ezDatetimePopControl', [
+	'$compile',
+	'$parse',
+	'$document',
+	'$position',
+  'EzDatetimeConfigService',
+  function(
+		$compile,
+		$parse,
+		$document,
+		$position,
+    ConfigService
+  ) {
+    return {
+      restrict: 'EA',
+      require: 'ngModel',
+			templateUrl: 'ez_datetime_range_popup.html',
+      scope: {
+	      isOpen: '=?',
+        ngModel: '=',
+        from: '=?',
+        to: '=?',
+        config: '=?'
+      },
+      link: function(scope, $element, attrs, ngModel) {
+        var rangeEnabled = false;
+	      scope.$watch('isOpen', function(value) {
+	        if (value) {
+	          scope.$broadcast('daterangepicker.focus');
+	          scope.position = appendToBody ? $position.offset(element) : $position.position(element);
+	          scope.position.top = scope.position.top + element.prop('offsetHeight');
+
+	          // $document.bind('click', documentClickBind);
+	        } else {
+	          // $document.unbind('click', documentClickBind);
+	        }
+	      });
+        scope.form = {};
+
+        $element.addClass('ez-datetime-control');
+
+        ConfigService.resolve(scope, attrs);
+
+        if (!!attrs.from && !!attrs.to) {
+          rangeEnabled = true;
+        }
+
+        ngModel.$formatters.push(function(v) {
+          if (v) {
+            if (rangeEnabled && scope.options.modelBinding === 'default') {
+              v = moment(v.from).format(scope.options.viewFormat) + ' - ' + moment(v.to).format(scope.options.viewFormat);
+            } else {
+              v = moment(v).format(scope.options.viewFormat);
+            }
+          }
+
+          return v;
+        });
+				ngModel.$render = function() {
+					$element.after(angular.element('<h1>***************</h1'));
+				}
+
+        $element.bind('click', function() {
+
+          scope.form.date = ngModel.$modelValue;
+
+          // try to init from ngModel value first
+          if (!!scope.form.date) {
+            if (!!scope.form.date.from) {
+              scope.form.from = scope.form.date.from;
+            }
+
+            if (!!scope.form.date.to) {
+              scope.form.to = scope.form.date.to;
+            }
+          }
+
+          // try to init from from/to scope attributes
+          //
+          scope.form.from = scope.from;
+          scope.form.to = scope.to;
+
+          scope.form.isFrom = !!attrs.to && !!scope.form.to;
+          scope.form.isTo = !!attrs.from && !!scope.form.from;
+
+          if (!scope.form.from && !scope.from) {
+            scope.form.from = moment().format(scope.options.modelFormat);
+          }
+
+          if (!scope.form.to && !scope.to) {
+            scope.form.to = moment().format(scope.options.modelFormat);
+          }
+					console.log('****************');
+          // $modal.open({
+          //   templateUrl: rangeEnabled ? 'ez_datetime_range_modal.html' : 'ez_datetime_modal.html',
+          //   controller: 'EzDatetimeModalController',
+          //   scope: scope,
+          // }).result.then(function() {
+          //   scope.from = scope.form.from;
+          //   scope.to = scope.form.to;
+          //
+          //   if (rangeEnabled) {
+          //     switch(scope.options.modelBinding) {
+          //     case 'default':
+          //       scope.ngModel = {
+          //         from: scope.from,
+          //         to: scope.to
+          //       };
+          //       break;
+          //     case 'from':
+          //       scope.ngModel = scope.from;
+          //       break;
+          //     case 'to':
+          //       scope.ngModel = scope.to;
+          //       break;
+          //     }
+          //   } else {
+          //     scope.ngModel = scope.form.date;
+          //   }
+          //
+          //   ngModel.$setDirty();
+          // });
+        });
+
+      }
+    };
+  }
+]);
 
 angular.module('ez.datetime').directive('ezTimePicker', [
   'EzDatetimeConfigService',
